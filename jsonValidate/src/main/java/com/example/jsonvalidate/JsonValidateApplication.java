3973f7b224fdf935248606f04d5adba33c3e426a
aldrichzeng <zengyao.zy@alibaba-inc.com>
2023-05-25 15:05:51 +0800

    学习JSON Schema Validation

---
+++ jsonValidate/src/main/java/com/example/jsonvalidate/JsonValidateApplication.java
@@ -0,0 +1,12 @@
+package com.example.jsonvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonValidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonValidateApplication.class, args);
+    }
+}
