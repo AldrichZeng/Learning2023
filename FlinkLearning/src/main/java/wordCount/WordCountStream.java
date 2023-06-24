cc7974e509587aebbded670929840cfa658e40c0
zengyaoIDEA <731692197@qq.com>
2023-06-24 10:51:27 +0800

    Flink

---
+++ FlinkLearning/src/main/java/wordCount/WordCountStream.java
@@ -0,0 +1,11 @@
+package wordCount;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordCountStream {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        
+        
+    }
+}
