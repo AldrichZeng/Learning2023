5ca219755576dc8e469af5f3d9891a3287dc8e8d
aldrichzeng <zengyao.zy@alibaba-inc.com>
2023-07-13 10:00:52 +0800

    我想要知道初始位点是否生效

--- DebeziumLearning/src/main/java/com/zy/LSNTest.java
+++ DebeziumLearning/src/main/java/com/zy/LSNTest.java
@@ -14,50 +14,7 @@ import org.springframework.core.io.ClassPathResource;
  * @Date: 2023/7/12 17:50
  */
 public class LSNTest {
-    public static void main(String[] args) throws IOException {
 
-        Lsn lsn = Lsn.valueOf(1161302121872L);
-        System.out.println(lsn.asString());
-        System.out.println(lsn.asLong());
-
-        lsn = Lsn.valueOf("10E/3C00AE20");
-        System.out.println(lsn.asLong());
-        System.out.println(lsn.asString());
-    }
-
-    public static void test2() throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource("startPosition.json");
-        InputStream inputStream = classPathResource.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-        String initialPosition = sb.toString();
-        br.close();
-
-        System.out.println(initialPosition);
-    }
-
-    public static void test() {
-        Lsn lsn = Lsn.valueOf(1161268569152L);
-        System.out.println(lsn.asString());
-        System.out.println(lsn.asLong());
-
-        System.out.println("==================");
-        ByteBuffer buf = ByteBuffer.allocate(8);
-        buf.putLong(1159741874432L);
-        buf.position(0);
-        System.out.println(buf.position());
-        int a = buf.getInt();
-        System.out.println(a);
-        System.out.println(String.format("%X", a));
-        System.out.println(buf.position());
-        int b = buf.getInt();
-        System.out.println(b);
-        System.out.println(String.format("%X", b));
-    }
 
     public static String lsnHexToDec(String lsnInHex) {
         Lsn lsn = Lsn.valueOf(lsnInHex);
---
+++ DebeziumLearning/src/test/java/com/zy/LSNTestTest.java
@@ -0,0 +1,66 @@
+package com.zy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+
+import io.debezium.connector.postgresql.connection.Lsn;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @author 匠承
+ * @Date: 2023/7/13 09:59
+ */
+public class LSNTestTest {
+
+    @Test
+    public void test1(){
+        Lsn lsn = Lsn.valueOf(1161268569152L);
+        System.out.println(lsn.asString());
+        System.out.println(lsn.asLong());
+
+        System.out.println("==================");
+        ByteBuffer buf = ByteBuffer.allocate(8);
+        buf.putLong(1159741874432L);
+        buf.position(0);
+        System.out.println(buf.position());
+        int a = buf.getInt();
+        System.out.println(a);
+        System.out.println(String.format("%X", a));
+        System.out.println(buf.position());
+        int b = buf.getInt();
+        System.out.println(b);
+        System.out.println(String.format("%X", b));
+    }
+
+    @Test
+    public void test2() throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("startPosition.json");
+        InputStream inputStream = classPathResource.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        String initialPosition = sb.toString();
+        br.close();
+
+        System.out.println(initialPosition);
+    }
+
+    @Test
+    public void test3(){
+        Lsn lsn = Lsn.valueOf(1161302121872L);
+        System.out.println(lsn.asString());
+        System.out.println(lsn.asLong());
+
+        lsn = Lsn.valueOf("10E/3C00AE20");
+        System.out.println(lsn.asLong());
+        System.out.println(lsn.asString());
+    }
+
+}
