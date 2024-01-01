f3adeb75644b532804cc6d05c10e920345c6ce64
aldrichzeng <zengyao.zy@alibaba-inc.com>
2024-01-01 11:27:39 +0800

    SpringLearning

---
+++ SpringLearning/src/main/java/com/example/springlearning/model/ValueObject.java
@@ -0,0 +1,35 @@
+package com.example.springlearning.model;
+
+/**
+ * @author 匠承
+ * @Date: 2023/5/16 23:13
+ */
+public class ValueObject {
+    private int id;
+    private String name;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
