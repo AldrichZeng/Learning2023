eb347c60849644503351634dc6941d1c68633561
aldrichzeng <zengyao.zy@alibaba-inc.com>
2024-01-28 11:04:37 +0800

    ThreadLocal学习

--- PgJdbc/src/main/java/com/zy/threadLocalLearning/MyTest.java
+++ PgJdbc/src/main/java/com/zy/threadLocalLearning/MyTest.java
@@ -13,6 +13,35 @@ public class MyTest {
 
     static String value;
 
+    public String getInstsanceValue() {
+        return intanceValue;
+    }
+
+    public void setIntanceValue(String intanceValue) {
+        this.intanceValue = intanceValue;
+    }
+
+    String intanceValue;
+
+    public MyTest() {
+        this.intanceValue = "hello";
+        this.instanceValueThreadLocal.set("hello");
+    }
+
+    ThreadLocal<String> instanceValueThreadLocal = new ThreadLocal<>();
+
+    public String getInstanceValueThreadLocal() {
+        return instanceValueThreadLocal.get();
+    }
+
+    public void setInstanceValueThreadLocal(String value) {
+        this.instanceValueThreadLocal.set(value);
+    }
+
+    public void removeInstanceValueThreadLocal() {
+        this.instanceValueThreadLocal.remove();
+    }
+
     /**
      * 使用ThreadLocal
      *
@@ -36,10 +65,13 @@ public class MyTest {
     }
 
     public static void main(String[] args) {
-        int n = 10;
+
+        MyTest test = new MyTest();
+        int n = 5;
         ExecutorService executorService = Executors.newFixedThreadPool(n);
 
-        for (int i = 0; i < n; i++) {
+        //for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n * 3; i++) {
             final int threadId = i;
             executorService.execute(new Runnable() {
                 @Override
@@ -49,7 +81,17 @@ public class MyTest {
                     //方式1：不使用ThreadLocal，直接赋值
                     //MyTest.value = tmp;
                     //方式2：通过ThreadLocal赋值
-                    threadLocal.set(tmp);
+                    //if (threadLocal.get() == null) {
+                    //    System.out.println("threadName:" + Thread.currentThread().getName() + ", successfully set value to: " + threadId);
+                    //    threadLocal.set(tmp);
+                    //}
+
+                    //方式3：验证实例变量是否线程安全
+                    //test.setIntanceValue(tmp);
+                    //方式4：验证实例变量ThreadLoca是否线程安全
+                    if (test.getInstanceValueThreadLocal() == null) {
+                        test.setInstanceValueThreadLocal(tmp);
+                    }
                     try {
                         // 加上sleep，可以发现静态变量会被不同线程修改，使得各个线程得到了非预期的结果。
                         Thread.sleep(100);
@@ -59,10 +101,16 @@ public class MyTest {
                     //方式1：不使用ThreadLocal，直接打印静态变量
                     //String result = print();
                     //方式2：使用ThreadLocal
-                    String result = printUsingThreadLocal();
+                    //String result = printUsingThreadLocal();
+                    //方式3：验证实例变量是否线程安全
+                    //String result = test.getInstsanceValue();
+                    //方式4：验证实例变量ThreadLocal是否线程安全
+                    String result = test.getInstanceValueThreadLocal();
                     int x = Integer.valueOf(result.substring(result.lastIndexOf("thread ") + 7));
                     System.out.println("threadName:" + Thread.currentThread().getName() + ",预期threadId" + threadId + ",实际threadId:" + x);
                     System.out.println(threadId == x);
+                    //threadLocal.remove();
+                    test.removeInstanceValueThreadLocal();
                 }
             });
         }
