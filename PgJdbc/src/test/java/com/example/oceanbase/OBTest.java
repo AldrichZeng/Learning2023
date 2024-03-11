5c0d4bb152eeb6894bedf6372a4af15502e6ecd5
aldrichzeng <zengyao.zy@alibaba-inc.com>
2024-03-11 22:20:44 +0800

    OB

--- PgJdbc/src/test/java/com/example/oceanbase/OBTest.java
+++ PgJdbc/src/test/java/com/example/oceanbase/OBTest.java
@@ -1,5 +1,6 @@
 package com.example.oceanbase;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson2.JSON;
 
@@ -54,15 +55,40 @@ public class OBTest {
     public void test3() throws ClientException {
         String regionId = "cn-beijing";
         DefaultProfile profile = DefaultProfile.getProfile(regionId, accessId, accessKey);
-        IAcsClient client = new DefaultAcsClient(profile);
 
-        CommonRequest commonRequest = new CommonRequest();
-        commonRequest.setSysDomain("oceanbasepro.cn-beijing.aliyuncs.com");
-        commonRequest.setSysAction("DescribeInstances");
-        commonRequest.setSysVersion("2019-09-01");
-        commonRequest.setSysRegionId(regionId);
-        CommonResponse response = client.getCommonResponse(commonRequest);
-        System.out.println(JSON.toJSONString(response.getData()));
+        int pageNumber = 1;
+        while (true) {
+            final int pageNumberFinal = pageNumber;
+            System.out.println("pageNumber: " + pageNumberFinal);
+            CommonRequest commonRequest = new CommonRequest();
+            IAcsClient client = new DefaultAcsClient(profile);
+            commonRequest.setSysDomain("oceanbasepro.cn-beijing.aliyuncs.com");
+            commonRequest.setSysAction("DescribeInstances");
+            commonRequest.setSysVersion("2019-09-01");
+            commonRequest.setSysRegionId(regionId);
+            commonRequest.putQueryParameter("PageSize", "1");
+            commonRequest.putBodyParameter("PageSize", "1");
+            commonRequest.putQueryParameter("PageNumber", String.valueOf(pageNumberFinal));
+            commonRequest.putBodyParameter("PageNumber", String.valueOf(pageNumberFinal));
+            commonRequest.putQueryParameter("SecurityToken", null);
+            commonRequest.putBodyParameter("SecurityToken", null);
+            CommonResponse response = client.getCommonResponse(commonRequest);
+            if (response != null && response.getData() != null) {
+                System.out.println("response.data = " + JSON.toJSONString(response.getData()));
+                JSONArray instances = JSONObject.parseObject(response.getData()).getJSONArray("Instances");
+                if (instances == null || instances.isEmpty()) {
+                    break;
+                }
+                for (Object instance : instances) {
+                    JSONObject instanceJSON = (JSONObject) instance;
+                    System.out.println("instanceId:" + instanceJSON.getString("InstanceId"));
+                    System.out.println("instanceName:" + instanceJSON.getString("InstanceName"));
+                }
+                pageNumber++;
+            } else {
+                break;
+            }
+        }
     }
 
     @Test
@@ -100,7 +126,13 @@ public class OBTest {
         CommonResponse response = client.getCommonResponse(commonRequest);
         System.out.println(JSON.toJSONString(response.getData()));
 
-        DescribeTenantResponse describeTenantResponse = JSONObject.parseObject(JSON.toJSONString(response.getData())).toJavaObject(DescribeTenantResponse.class);
-        System.out.println(JSON.toJSONString(describeTenantResponse));
+        //DescribeTenantResponse describeTenantResponse = JSONObject.parseObject(JSON.toJSONString(response.getData())).toJavaObject(DescribeTenantResponse.class);
+        //System.out.println(JSON.toJSONString(describeTenantResponse));
+    }
+
+    @Test
+    public void test6() {
+        String a = "jdbc:oceanbase:";
+        System.out.println(a.substring("jdbc:".length(), a.length() - 1));
     }
 }
