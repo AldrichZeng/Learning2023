ee794082989046ac8d88fe1d86b7643b538a0543
aldrichzeng <zengyao.zy@alibaba-inc.com>
2024-11-02 20:34:06 +0800

    shiroLearning

---
+++ shiroLearning/src/main/java/com/zy/Main.java
@@ -0,0 +1,11 @@
+package com.zy;
+
+/**
+ * @author 匠承
+ * @Date: 2024/11/2 20:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
